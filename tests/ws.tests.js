00a2895d5a2d57ffd1519fd8b6bdbcaf376038a4
Kevin Jahns <kevin.jahns@protonmail.com>
2024-02-15 10:17:52 +0100

    added more tests

--- tests/ws.tests.js
+++ tests/ws.tests.js
@@ -59,9 +59,10 @@ const createTestCase = async tc => {
   const redisClient = redis.createClient({ url: redisUrl })
   await redisClient.connect()
   await redisClient.flushAll()
-  await redisClient.quit()
+  prevClients.push({ destroy: () => redisClient.quit().then(() => {}) })
   const [apiClient, server, worker] = await promise.all([createApiClient(), createServer(), createWorker()])
   return {
+    redisClient,
     apiClient,
     server,
     worker,
@@ -83,14 +84,35 @@ const waitDocsSynced = (ydoc1, ydoc2) =>
 /**
  * @param {t.TestCase} tc
  */
-export const testUpdateApiMessages = async tc => {
-  const { createWsClient } = await createTestCase(tc)
+export const testSyncAndCleanup = async tc => {
+  const { createWsClient, worker, redisClient } = await createTestCase(tc)
   const { ydoc: doc1 } = createWsClient('map')
+  // doc2: can retrieve changes propagated on stream
   const { ydoc: doc2 } = createWsClient('map')
   doc1.getMap().set('a', 1)
   await waitDocsSynced(doc1, doc2)
+  const docStreamExistsBefore = await redisClient.exists(api.computeRedisRoomStreamName(tc.testName + '-' + 'map', 'index'))
   t.assert(doc2.getMap().get('a') === 1)
+  // doc3 can retrieve older changes from stream
   const { ydoc: doc3 } = createWsClient('map')
   await waitDocsSynced(doc1, doc3)
   t.assert(doc3.getMap().get('a') === 1)
+  await promise.wait(worker.client.redisMinMessageLifetime * 2)
+  const docStreamExists = await redisClient.exists(api.computeRedisRoomStreamName(tc.testName + '-' + 'map', 'index'))
+  const workerLen = await redisClient.xLen('y:worker')
+  t.assert(!docStreamExists && docStreamExistsBefore)
+  t.assert(workerLen === 0)
+  // doc4 can retrieve the document again from MemoryStore
+  const { ydoc: doc4 } = createWsClient('map')
+  await waitDocsSynced(doc3, doc4)
+  t.assert(doc3.getMap().get('a') === 1)
+  const memRetrieved = await store.retrieveDoc(tc.testName + '-' + 'map', 'index')
+  t.assert(memRetrieved?.references.length === 1)
+  // now write another updates that the worker will collect
+  doc1.getMap().set('a', 2)
+  await promise.wait(worker.client.redisMinMessageLifetime * 2)
+  t.assert(doc2.getMap().get('a') === 2)
+  const memRetrieved2 = await store.retrieveDoc(tc.testName + '-' + 'map', 'index')
+  // should delete old references
+  t.assert(memRetrieved2?.references.length === 1)
 }
