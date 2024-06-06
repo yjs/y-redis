d8093e3401d9470f4c063ae27f981800379f7f68
Miguel Laginha <me@miguellaginha.com>
2024-06-06 12:59:57 +0100

    fix: use ESM imports instead

--- instrument-worker.js
+++ instrument-worker.js
@@ -1,5 +1,5 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-const Sentry = require("@sentry/node");
+import * as Sentry from "@sentry/node";
 const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
