81ffa097f86b89192b23045eac0ccee1039963cb
Miguel Laginha <me@miguellaginha.com>
2024-07-22 09:52:25 +0100

    fix: if statement was misplaced

--- instrument-server.js
+++ instrument-server.js
@@ -2,14 +2,14 @@
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
-Sentry.init({
-  dsn: "https://f453943efb537f4ce3661c7e9482c3ed@o4506576222289920.ingest.us.sentry.io/4507221379383296",
-  integrations: [nodeProfilingIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
 if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://f453943efb537f4ce3661c7e9482c3ed@o4506576222289920.ingest.us.sentry.io/4507221379383296",
+    integrations: [nodeProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
 }
--- instrument-worker.js
+++ instrument-worker.js
@@ -2,14 +2,13 @@
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
-Sentry.init({
-  dsn: "https://66fe40b298953e7d4718f567cf20f579@o4506576222289920.ingest.us.sentry.io/4507221426962432",
-  integrations: [nodeProfilingIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
 if (process.env.NODE_ENV === "production") {
-
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+  Sentry.init({
+    dsn: "https://66fe40b298953e7d4718f567cf20f579@o4506576222289920.ingest.us.sentry.io/4507221426962432",
+    integrations: [nodeProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
 }
