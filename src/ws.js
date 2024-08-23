cce3345f9fab5ec657a6de0c3c6f86c85fa83d22
dyedwiper <maximilian.becker@dataport.de>
2024-08-23 18:09:09 +0200

    Add try-catch blocks

--- src/ws.js
+++ src/ws.js
@@ -123,106 +123,129 @@ export const registerYWebsocketServer = async (
     idleTimeout: 60,
     sendPingsAutomatically: true,
     upgrade: async (res, req, context) => {
-      const url = req.getUrl()
-      const headerWsKey = req.getHeader('sec-websocket-key')
-      const headerWsProtocol = req.getHeader('sec-websocket-protocol')
-      const headerWsExtensions = req.getHeader('sec-websocket-extensions')
-      let aborted = false
-      res.onAborted(() => {
-        console.log('Upgrading client aborted', { url })
-        aborted = true
-      })
       try {
-        const { hasWriteAccess, room, userid } = await checkAuth(req)
-        if (aborted) return
-        res.cork(() => {
-          res.upgrade(
-            new User(room, hasWriteAccess, userid),
-            headerWsKey,
-            headerWsProtocol,
-            headerWsExtensions,
-            context
-          )
+        const url = req.getUrl()
+        const headerWsKey = req.getHeader('sec-websocket-key')
+        const headerWsProtocol = req.getHeader('sec-websocket-protocol')
+        const headerWsExtensions = req.getHeader('sec-websocket-extensions')
+        let aborted = false
+        res.onAborted(() => {
+          console.log('Upgrading client aborted', { url })
+          aborted = true
         })
-      } catch (err) {
-        console.log(`Failed to auth to endpoint ${url}`, err)
-        if (aborted) return
+        try {
+          const { hasWriteAccess, room, userid } = await checkAuth(req)
+          if (aborted) return
+          res.cork(() => {
+            res.upgrade(
+              new User(room, hasWriteAccess, userid),
+              headerWsKey,
+              headerWsProtocol,
+              headerWsExtensions,
+              context
+            )
+          })
+        } catch (err) {
+          console.log(`Failed to auth to endpoint ${url}`, err)
+          if (aborted) return
+          res.cork(() => {
+            res.writeStatus('401 Unauthorized').end('Unauthorized')
+          })
+        }
+      } catch (error) {
         res.cork(() => {
-          res.writeStatus('401 Unauthorized').end('Unauthorized')
+          res.writeStatus('500 Internal Server Error').end('Internal Server Error')
         })
+        console.error(error)
       }
     },
     open: async (ws) => {
-      const user = ws.getUserData()
-      log(() => ['client connected (uid=', user.id, ', ip=', Buffer.from(ws.getRemoteAddressAsText()).toString(), ')'])
-      openWsCallback(ws);
-      const stream = api.computeRedisRoomStreamName(user.room, 'index', redisPrefix)
-      user.subs.add(stream)
-      ws.subscribe(stream)
-      user.initialRedisSubId = subscriber.subscribe(stream, redisMessageSubscriber).redisId
-      const indexDoc = await client.getDoc(user.room, 'index')
-      if (indexDoc.ydoc.store.clients.size === 0) {
-        initDocCallback(user.room, 'index', client)
-      }
-      if (user.isClosed) return
-      ws.cork(() => {
-        ws.send(protocol.encodeSyncStep1(Y.encodeStateVector(indexDoc.ydoc)), true, false)
-        ws.send(protocol.encodeSyncStep2(Y.encodeStateAsUpdate(indexDoc.ydoc)), true, true)
-        if (indexDoc.awareness.states.size > 0) {
-          ws.send(protocol.encodeAwarenessUpdate(indexDoc.awareness, array.from(indexDoc.awareness.states.keys())), true, true)
+      try {
+        const user = ws.getUserData()
+        log(() => ['client connected (uid=', user.id, ', ip=', Buffer.from(ws.getRemoteAddressAsText()).toString(), ')'])
+        openWsCallback(ws);
+        const stream = api.computeRedisRoomStreamName(user.room, 'index', redisPrefix)
+        user.subs.add(stream)
+        ws.subscribe(stream)
+        user.initialRedisSubId = subscriber.subscribe(stream, redisMessageSubscriber).redisId
+        const indexDoc = await client.getDoc(user.room, 'index')
+        if (indexDoc.ydoc.store.clients.size === 0) {
+          initDocCallback(user.room, 'index', client)
         }
-      })
+        if (user.isClosed) return
+        ws.cork(() => {
+          ws.send(protocol.encodeSyncStep1(Y.encodeStateVector(indexDoc.ydoc)), true, false)
+          ws.send(protocol.encodeSyncStep2(Y.encodeStateAsUpdate(indexDoc.ydoc)), true, true)
+          if (indexDoc.awareness.states.size > 0) {
+            ws.send(protocol.encodeAwarenessUpdate(indexDoc.awareness, array.from(indexDoc.awareness.states.keys())), true, true)
+          }
+        })
 
-      // awareness is destroyed here to avoid memory leaks, see: https://github.com/yjs/y-redis/issues/24
-      indexDoc.awareness.destroy()
+        // awareness is destroyed here to avoid memory leaks, see: https://github.com/yjs/y-redis/issues/24
+        indexDoc.awareness.destroy()
 
-      if (api.isSmallerRedisId(indexDoc.redisLastId, user.initialRedisSubId)) {
-        // our subscription is newer than the content that we received from the api
-        // need to renew subscription id and make sure that we catch the latest content.
-        subscriber.ensureSubId(stream, indexDoc.redisLastId)
+        if (api.isSmallerRedisId(indexDoc.redisLastId, user.initialRedisSubId)) {
+          // our subscription is newer than the content that we received from the api
+          // need to renew subscription id and make sure that we catch the latest content.
+          subscriber.ensureSubId(stream, indexDoc.redisLastId)
+        }
+      } catch (error) {
+        console.error(error)
+        // Schulcloud specific: This close code is currently used in our client for Internal Server Error
+        ws.end(4500)
       }
     },
     message: (ws, messageBuffer) => {
-      const user = ws.getUserData()
-      // don't read any messages from users without write access
-      if (!user.hasWriteAccess) return
-      // It is important to copy the data here
-      const message = Buffer.from(messageBuffer.slice(0, messageBuffer.byteLength))
-      if ( // filter out messages that we simply want to propagate to all clients
-        // sync update or sync step 2
-        (message[0] === protocol.messageSync && (message[1] === protocol.messageSyncUpdate || message[1] === protocol.messageSyncStep2)) ||
-        // awareness update
-        message[0] === protocol.messageAwareness
-      ) {
-        if (message[0] === protocol.messageAwareness) {
-          const decoder = decoding.createDecoder(message)
-          decoding.readVarUint(decoder) // read message type
-          decoding.readVarUint(decoder) // read length of awareness update
-          const alen = decoding.readVarUint(decoder) // number of awareness updates
-          const awId = decoding.readVarUint(decoder)
-          if (alen === 1 && (user.awarenessId === null || user.awarenessId === awId)) { // only update awareness if len=1
-            user.awarenessId = awId
-            user.awarenessLastClock = decoding.readVarUint(decoder)
+      try {
+        const user = ws.getUserData()
+        // don't read any messages from users without write access
+        if (!user.hasWriteAccess) return
+        // It is important to copy the data here
+        const message = Buffer.from(messageBuffer.slice(0, messageBuffer.byteLength))
+        if ( // filter out messages that we simply want to propagate to all clients
+          // sync update or sync step 2
+          (message[0] === protocol.messageSync && (message[1] === protocol.messageSyncUpdate || message[1] === protocol.messageSyncStep2)) ||
+          // awareness update
+          message[0] === protocol.messageAwareness
+        ) {
+          if (message[0] === protocol.messageAwareness) {
+            const decoder = decoding.createDecoder(message)
+            decoding.readVarUint(decoder) // read message type
+            decoding.readVarUint(decoder) // read length of awareness update
+            const alen = decoding.readVarUint(decoder) // number of awareness updates
+            const awId = decoding.readVarUint(decoder)
+            if (alen === 1 && (user.awarenessId === null || user.awarenessId === awId)) { // only update awareness if len=1
+              user.awarenessId = awId
+              user.awarenessLastClock = decoding.readVarUint(decoder)
+            }
           }
+          client.addMessage(user.room, 'index', message)
+        } else if (message[0] === protocol.messageSync && message[1] === protocol.messageSyncStep1) { // sync step 1
+          // can be safely ignored because we send the full initial state at the beginning
+        } else {
+          console.error('Unexpected message type', message)
         }
-        client.addMessage(user.room, 'index', message)
-      } else if (message[0] === protocol.messageSync && message[1] === protocol.messageSyncStep1) { // sync step 1
-        // can be safely ignored because we send the full initial state at the beginning
-      } else {
-        console.error('Unexpected message type', message)
+      } catch (error) {
+        console.error(error)
+        // Schulcloud specific: This close code is currently used in our client for Internal Server Error
+        ws.end(4500)
       }
     },
     close: (ws, code, message) => {
-      const user = ws.getUserData()
-      user.awarenessId && client.addMessage(user.room, 'index', Buffer.from(protocol.encodeAwarenessUserDisconnected(user.awarenessId, user.awarenessLastClock)))
-      user.isClosed = true
-      log(() => ['client connection closed (uid=', user.id, ', code=', code, ', message="', Buffer.from(message).toString(), '")'])
-      closeWsCallback(ws, code, message);
-      user.subs.forEach(topic => {
-        if (app.numSubscribers(topic) === 0) {
-          subscriber.unsubscribe(topic, redisMessageSubscriber)
-        }
-      })
+      try {
+        const user = ws.getUserData()
+        user.awarenessId && client.addMessage(user.room, 'index', Buffer.from(protocol.encodeAwarenessUserDisconnected(user.awarenessId, user.awarenessLastClock)))
+        user.isClosed = true
+        log(() => ['client connection closed (uid=', user.id, ', code=', code, ', message="', Buffer.from(message).toString(), '")'])
+        closeWsCallback(ws, code, message);
+        user.subs.forEach(topic => {
+          if (app.numSubscribers(topic) === 0) {
+            subscriber.unsubscribe(topic, redisMessageSubscriber)
+          }
+        })
+      } catch (error) {
+        console.error(error)
+      }
     }
   }))
   return new YWebsocketServer(app, client, subscriber)
